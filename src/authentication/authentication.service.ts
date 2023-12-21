a092d8929ae78f3d1c4d19ed2bd822dd7db376fc
lakmal05 <lakmaljayawardhana59@gmail.com>
2023-12-21 16:19:36 +0530

    passord

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -7,36 +7,40 @@ export class AuthenticationService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async login(data: any) {
+    const incoming_passowrd = data.password;
     const is_exsist = await this.prismaService.user.findUnique({
       where: {
         username: data.username,
-        password: data.password,
       },
     });
-
+    const password = is_exsist.password;
     if (!is_exsist) {
       return false;
     } else {
-      const employee = await this.prismaService.employee.findUnique({
-        where: {
-          id: is_exsist.employee_id,
-        },
-      });
-      const token = await this.generateToken(employee.emp_id);
+      if (is_exsist.password !== incoming_passowrd) {
+        return false;
+      } else {
+        const employee = await this.prismaService.employee.findUnique({
+          where: {
+            id: is_exsist.employee_id,
+          },
+        });
+        const token = await this.generateToken(employee.emp_id);
 
-      const data = {
-        id: employee.id,
-        emp_id: employee.emp_id,
-        first_name: employee.first_name,
-        last_name: employee.last_name,
-        nic: employee.nic,
-        type: employee.type,
-        contact_no: employee.contact_no,
-        created_at: employee.created_at,
-        updated_at: employee.updated_at,
-        token: token,
-      };
-      return data;
+        const data = {
+          id: employee.id,
+          emp_id: employee.emp_id,
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          nic: employee.nic,
+          type: employee.type,
+          contact_no: employee.contact_no,
+          created_at: employee.created_at,
+          updated_at: employee.updated_at,
+          token: token,
+        };
+        return data;
+      }
     }
   }
 
